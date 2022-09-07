da3e1a8f888a017fa9e4a6a70a6e017ee0804899
JungMin Park <aossuper7@naver.com>
2022-09-07 17:20:50 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    std::ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b;
+}

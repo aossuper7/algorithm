1c4667e657b37221158c2bd35901023b958a547c
JungMin Park <aossuper7@naver.com>
2022-09-14 16:41:40 +0900

    [Gold IV] Title: 오큰수, Time: 252 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	stack<int> s;
+	int n;
+	cin >> n;
+	vector<int> A(n);
+	vector<int> ans(n);
+	for (int i = 0; i < n; i++)
+		cin >> A[i];
+
+	s.push(0);
+	for (int i = 1; i < n; i++) {
+		if (s.empty()) s.push(i);
+		while (!s.empty() && A[s.top()] < A[i]) {
+			ans[s.top()] = A[i];
+			s.pop();
+		}
+		s.push(i);
+	}
+	while (!s.empty()) {
+		ans[s.top()] = -1;
+		s.pop();
+	}
+	for (int i = 0; i < n; i++)
+		cout << ans[i] << ' ';
+}

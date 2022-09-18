c64dafb345619f01f202b0dd582d849f0bc97e8e
JungMin Park <aossuper7@naver.com>
2022-09-18 22:04:47 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int c;
+	cin >> c;
+	for (int i = 1; i <= c; i++)
+		cout << "Hello World, Judge " << i << "!\n";
+}

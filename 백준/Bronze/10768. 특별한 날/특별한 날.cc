9801c8971883f5aa85c235b57ffa6171e98dfabc
JungMin Park <aossuper7@naver.com>
2022-09-22 15:04:08 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10768. 특별한 날/특별한 날.cc
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -3,14 +3,12 @@ using namespace std;
 int main() {
 	int a, b;
 	cin >> a >> b;
-	if (a < 2)
+	if (a == 2 && b == 18)
+		cout << "Special";
+	else if (a < 2)
 		cout << "Before";
-	else if (a > 2)
+	else if (a == 2 && b < 18)
+		cout << "Before";
+	else
 		cout << "After";
-	else if (a == 2)
-		if (b <= 17)
-			cout << "Before";
-		else if (b > 18)
-			cout << "After";
-		else cout << "Special";
 }

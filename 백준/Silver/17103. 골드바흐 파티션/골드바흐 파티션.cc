f75d53bb2d454e07286ea149e6ed4ec915aa886d
JungMin Park <aossuper7@naver.com>
2022-09-27 21:35:31 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 12 ms, Memory: 3892 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool check[1000001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> primes;
+    for (int i = 2; i <= 1000000; i++) {
+        if (check[i] == false) {
+            primes.push_back(i);
+            for (int j = i + i; j <= 1000000; j += i) {
+                check[j] = true;
+            }
+        }
+    }
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int ans = 0;
+        for (int i = 0; i < primes.size(); i++) {
+            if (n - primes[i] >= 2 && primes[i] <= n - primes[i]) {
+                if (check[n - primes[i]] == false) {
+                    ans += 1;
+                }
+            }
+            else {
+                break;
+            }
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}

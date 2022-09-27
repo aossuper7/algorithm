829c85a8249653eca1e1a3d2027eeef8c798f705
JungMin Park <aossuper7@naver.com>
2022-09-27 09:46:25 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -6,16 +6,8 @@ int main() {
 	long long n, m, i, t = 0, f = 0;
 	cin >> n >> m;
 	for (i = 2; i <= n; i *= 2)
-		t += n / i;
-	for (i = 2; i <= n - m; i *= 2)
-		t -= (n - m) / i;
-	for (i = 2; i <= m; i *= 2)
-		t -= m / i;
+		t += n / i - m / i - (n - m) / i;
 	for (i = 5; i <= n; i *= 5)
-		f += n / i;
-	for (i = 5; i <= n - m; i *= 5)
-		f -= (n - m) / i;
-	for (i = 5; i <= m; i *= 5)
-		f -= m / i;
+		f += n / i - m / i - (n - m) / i;
 	cout << min(t, f);
 }

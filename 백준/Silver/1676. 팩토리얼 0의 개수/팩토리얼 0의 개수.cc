6fe58d6553ea3e0e35e8fd7497b2756a31c2110d
JungMin Park <aossuper7@naver.com>
2022-09-27 09:14:11 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int n, s = 0;
+	cin >> n;
+	for (int i = 5; i <= n; i *= 5) {
+		s += n / i;
+	}
+	cout << s;
+}

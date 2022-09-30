d40c8a64b0b6d174b6fdfe07c951a2a30535cc3b
JungMin Park <aossuper7@naver.com>
2022-09-30 19:44:52 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int d[1001];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int n;
+	cin >> n;
+	d[0] = d[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		d[i] = d[i - 1] + d[i - 2] + d[i-2];
+		d[i] %= 10007;
+	}
+	cout << d[n];
+}
